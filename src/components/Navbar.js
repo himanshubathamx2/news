dfa2ce82c2c6fbc070d5f156a0574f091de521b9
Himanshu Batham <himanshubatham.official@gmail.com>
2024-05-22 11:55:43 +0530

    home page changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export default class Navbar extends Component {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className="nav-link " aria-current="page" to="/">
-                  Home
+                  Home Page
                 </Link>
               </li>
               <li className="nav-item">
